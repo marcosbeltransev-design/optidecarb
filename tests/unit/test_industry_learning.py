84740b6abe4be70c591eb4e32e9873b3c08d175f
marcosbeltransev-design <marcosbeltransev@gmail.com>
2026-09-08 19:49:23 +0200

    test: align industry learning version with 1.3.1

--- tests/unit/test_industry_learning.py
+++ tests/unit/test_industry_learning.py
@@ -18,7 +18,7 @@ from industrial_energy_lab.utils.version import OPTIMIZATION_MODEL_VERSION
 
 
 def test_app_version_does_not_change_engine_version():
-    assert APP_VERSION == "1.3.0"
+    assert APP_VERSION == "1.3.1"
     assert OPTIMIZATION_MODEL_VERSION == "0.3.0"
 
 
